edd249c9e3071ee49487ee044778f980bfee3f23
calgaba <calgaba.practicas@gmail.com>
2025-11-11 22:16:36 +0100

    navbar and routing

--- src/app/pages/request/request-detail/request-detail.component.ts
+++ src/app/pages/request/request-detail/request-detail.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-request-detail',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   styleUrl: './request-detail.component.css',
   templateUrl: './request-detail.component.html',
 })
