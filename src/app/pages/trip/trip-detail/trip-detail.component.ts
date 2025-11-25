efac45ee3259f55945300ea1079ce4213b5155e5
GaizkaCalvo <gaizkablusti@gmail.com>
2025-11-25 21:39:52 +0100

    Empezando la maquetacion del trip-details

--- src/app/pages/trip/trip-detail/trip-detail.component.ts
+++ src/app/pages/trip/trip-detail/trip-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { TripApiService } from '../../../services/api-rest/trip-rest.service';
+import { ITrip } from '../../../interfaces/ITrip';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-trip-detail',
@@ -8,6 +11,34 @@ import { Component } from '@angular/core';
 })
 export class TripDetailComponent 
 {
+  tripService = inject(TripApiService);
+	activatedRoute = inject(ActivatedRoute);
+
+	tripId! : number;
+  tripInfo! : ITrip;
   image : string = "https://www.mercurynews.com/wp-content/uploads/2021/04/SJM-L-ROADTRIP-0502-01.jpg?w=1024";
+
   
+  ngOnInit()
+  {
+		//Get the tripId
+		this.activatedRoute.params.subscribe(params => 
+		{
+ 			this.tripId = params['id'];
+ 		});
+
+    this.loadTrip();
+  }
+
+  async loadTrip()
+  {
+    try 
+    {  
+      this.tripInfo = await this.tripService.getTrip(this.tripId);
+    } 
+    catch (error) 
+    {
+      console.log('Couldn`t get the trip info:' + error);
+    }
+  }
 }
